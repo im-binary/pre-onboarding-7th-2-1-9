483c2efe1eff734ec1421e41e355d894cd88fb0f
pongdang <pongpong0121@kakao.com>
2022-11-02 21:08:28 +0900

    ⚒ refactor : Header 컴포넌트 스타일

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,15 @@
+import styled from '@emotion/styled';
 import { ReactNode } from 'react';
+import { Theme } from '../styles/Theme';
 
 export function Header({ children }: { children: ReactNode }) {
-  return <header>{children}</header>;
+  return <HeaderBar>{children}</HeaderBar>;
 }
+
+const HeaderBar = styled.header`
+  padding: 20px;
+  text-align: center;
+  font-size: 1.7rem;
+  border-bottom: 1px solid ${Theme.mainColor};
+  font-weight: bold;
+`;
