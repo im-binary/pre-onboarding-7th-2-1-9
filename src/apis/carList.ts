c08876858b4740fb1c1b534fb1f1a3dda60e2be5
pongdang <pongpong0121@kakao.com>
2022-11-02 19:21:04 +0900

    ⚒ refactor : 사용하지 않는 함수 제거

--- src/apis/carList.ts
+++ src/apis/carList.ts
@@ -16,39 +16,3 @@ export const getCategoryCarList = async ({ segment }: { segment: string }) => {
 
   return data.payload;
 };
-
-export const getSmallSizeCarList = async ({ segment }: { segment: string }) => {
-  const { data } = await get({
-    url: `/cars`,
-    qsData: { segment },
-  });
-
-  return data.payload;
-};
-
-export const getMidSizeCarList = async ({ segment }: { segment: string }) => {
-  const { data } = await get({
-    url: `/cars`,
-    qsData: { segment },
-  });
-
-  return data.payload;
-};
-
-export const getFullSizeCarList = async ({ segment }: { segment: string }) => {
-  const { data } = await get({
-    url: `/cars`,
-    qsData: { segment },
-  });
-
-  return data.payload;
-};
-
-export const getSuvCarList = async ({ segment }: { segment: string }) => {
-  const { data } = await get({
-    url: `/cars`,
-    qsData: { segment },
-  });
-
-  return data.payload;
-};
