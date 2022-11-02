2ca5276ae446c9b34630597db3768fa3a6533962
pongdang <pongpong0121@kakao.com>
2022-11-02 13:17:01 +0900

    ⭐ feat : 현재 경로에 class를 추가해주는 컴포넌트 생성

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export function NavLink({
+  href,
+  children,
+  className,
+  scroll,
+  replace,
+}: {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+  scroll?: boolean;
+  replace?: boolean;
+}) {
+  const { asPath } = useRouter();
+
+  const isActive = asPath === href;
+
+  if (isActive) {
+    if (className == null) {
+      className = 'active';
+    } else {
+      className += ' active';
+    }
+  }
+
+  return (
+    <Link legacyBehavior href={href} scroll={scroll} replace={replace}>
+      {children}
+    </Link>
+  );
+}
