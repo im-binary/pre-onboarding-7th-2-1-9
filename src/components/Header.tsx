e3b53f39432c0dad0fd2c1c1835d032892687ae7
pongdang <pongpong0121@kakao.com>
2022-11-03 04:18:18 +0900

    ⭐ feat: header에 뒤로가기 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,24 @@ import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 import { Theme } from '../styles/Theme';
 
-export function Header({ children }: { children: ReactNode }) {
-  return <HeaderBar>{children}</HeaderBar>;
+export function Header({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
+  return <HeaderBar className={className}>{children}</HeaderBar>;
 }
 
 const HeaderBar = styled.header`
+  position: relative;
   padding: 20px;
   text-align: center;
-  font-size: 1.7rem;
   border-bottom: 1px solid ${Theme.mainColor};
   font-weight: bold;
+
+  h1 {
+    font-size: 1.7rem;
+  }
 `;
