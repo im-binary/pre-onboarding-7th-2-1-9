80c8e23f2530f2e7f48cf6d7a1e032819302d353
pongdang <pongpong0121@kakao.com>
2022-11-03 05:17:52 +0900

    ⭐ feat: 사용하지 않는 config 삭제

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['interview.platdev.net'],
-    loader: 'imgix',
-    path: 'https://example.com/myaccount/',
-  },
   async redirects() {
     return [
       {
