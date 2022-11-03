8dab0b1dd95517004020df3476c8071010112c20
pongdang <pongpong0121@kakao.com>
2022-11-03 11:50:47 +0900

    ⭐ feat: SEO를 위해 meta 태그 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="og:site_name" content="차량 대여 서비스" />
         <meta name="og:title" content="차량 대여 서비스" />
         <meta name="og:description" content="차량을 대여해보세요" />
-        <meta name="og:image" content="/images/driving-cat.png" />
+        <meta
+          name="og:image"
+          content="https://user-images.githubusercontent.com/76990149/199638933-76673587-4e66-49ad-9f8e-f8aedfaba914.png"
+        />
       </Head>
       <GlobalStyle />
       <ErrorBoundary
