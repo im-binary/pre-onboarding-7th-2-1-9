845c7a7334b6be8949669357b21029a26670debd
pongdang <pongpong0121@kakao.com>
2022-11-03 12:19:25 +0900

    ⭐ feat: SEO를 위해 meta 태그 적용

--- src/components/car/CarDetail.tsx
+++ src/components/car/CarDetail.tsx
@@ -14,7 +14,7 @@ export default function CarDetail() {
   const carList = useCarList({});
   const carDetail = carList.filter((x) => String(x.id) === carId);
 
-  const { attribute, amount, startDate, insurance, additionalProducts } =
+  const { id, attribute, amount, startDate, insurance, additionalProducts } =
     carDetail[0];
 
   return (
@@ -23,7 +23,27 @@ export default function CarDetail() {
         <title>
           {attribute.brand} - {attribute.name} | 차량 대여 서비스
         </title>
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`https://pre-onboarding-7th-2-1-9-two.vercel.app/${id}`}
+        />
+        <meta
+          property="og:title"
+          content={`${attribute.brand} - ${attribute.name} | 차량 대여 서비스`}
+        />
+        <meta property="og:image" content={`${attribute.imageUrl}`} />
+        <meta
+          property="og:description"
+          content={`${attribute.brand} | ${attribute.name} 월 ${amount} 원`}
+        />
+        <meta property="og:site_name" content="차량 대여 서비스" />
+        <meta property="og:locale" content="en_KO" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
       </Head>
+
       <article
         css={css`
           margin-bottom: 20px;
